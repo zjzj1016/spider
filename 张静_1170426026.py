b9e69327bd4ad11186e53d921419e14a195b1500
zjzj1016 <51935777+zjzj1016@users.noreply.github.com>
2019-06-20 17:20:58 +0800

    Add files via upload

--- 张静_1170426026.py
+++ 张静_1170426026.py
@@ -1,14 +1,5 @@
 import urllib.request
-import urllib.parse
-data = urllib.parse.urlencode({'wd':'啦啦啦'})
-print(data)
-url = 'http://www.baidu.com/s?' + data
+url = ('http://www.17k.com/chapter/2933095/36699279.html')
 response = urllib.request.urlopen(url)
 HTML = response.read().decode('utf8')
 print(HTML)
-
-data = bytes(urllib.parse.urlencode({'pw':'zj2456','acc':'2456752'}),enconding='utf8')
-url = 'http://qzone.qq.com/post'
-response = urllib.request.urlopen(url,data=data)
-result = response.read().decode('utf8')
-print(result)
